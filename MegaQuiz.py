5f37288a482746c96ea2236e44d3f5d3b07aa658
ibrahimijc <ibrahimjawedijc@gmail.com>
2020-01-11 00:17:26 +0500

    assignment file uploading

---
+++ MegaQuiz.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 22:44:42 2020
+
+@author: AI
+"""
+
+"""
+                                        MCQ's
+
+1) a,d
+2) b
+3) b
+4) d
+5) d
+6) b
+7) a,b
+8) b
+9) program
+10) program
+11) ['Feb', 'Apr', 'Mar']
+    ['Mar', 'May', 'Jun', 'Jul', 'Aug', 'Jan']
+    ['Jun', 'Jul', 'Aug', 'Jan']
+    ['Apr', 'May', 'Jul']
+    []
+    ['Apr', 'Mar', 'May', 'Jun', 'Jul', 'Aug']
+    ['Jan', 'Jul', 'May', 'Apr']
+    ['Feb', 'Apr', 'Mar', 'May']
+12) .remove(9)
+13) *
+14) 1) .upper()
+15) 'bool'
+    'str'
+    'int'
+    'float'
+16) p and R2
+17) p.X()
+18) 0
+    2.0
+    2
+    2
+19) for i in x:
+     for j in i:
+       print(j)
+20) for j, z in enumerate (q) 
+21) b) The keys of a dictionary can be accessed using values
+22) {0: 1, 1: 1, 2: 1}
+23) True
+    False
+    True
+24) 11.333333333333334
+25) name is: AliMarks: 44.5 
+    Reason : variable number of  parameters can only be accepted at the end of parameters
+    new function signature : def avg (  name, *opt_values )
+    function call : ( 'Ali', 5 , 9 , 20, 34, 87, 112  )
+26) The winner was Manchester
+    The score was 1-0
+27) Required or must parameters cannot be accepted after optional(variable) parameters.
+28) 000123
+29) 22.19
+30) No output
+31) No output. condition always fails
+32) a a a a a a 
+33) d
+    c
+    b
+    a
+34) 0
+    1
+    2
+    3
+    4
+35) 48 (multiplying any no with y)
+36) i=0 because a value declared and changed in the scope of function
+    doesn't effects global values
+37-39) Questions are missing
+40) <class 'tuple'> (variable(optional parameters are received as tuple))
+    (2, 3, 4) (value of the tuple)
+41) <class 'dict'> when named parameters are passed to the function they must be
+                   catched with **(double stars) and are of type dict
+42) 0: [0]
+    1: [0, 1]
+    2: [0, 1, 2]
+43) Brackets,Exponents ,Multiplication, Addition / Subtraction
+44) 
+45) 
+46) 
+    
+"""
+
+
